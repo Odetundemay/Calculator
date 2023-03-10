f94ff187cb24d81469ba6d7be2d9c751886e8548
Odetundemay <88235457+Odetundemay@users.noreply.github.com>
2023-03-10 16:47:34 +0100

    calculator with dark mode

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ThemeContext } from "../context/ThemeContext";
+import { Styles } from "../styles/GlobalStyles";
+
+interface ButtonProps {
+  onPress: () => void;
+  title: string;
+  isOrange?: boolean;
+  isGray?: boolean;
+}
+
+export default function Button({
+  title,
+  onPress,
+  isOrange,
+  isGray,
+}: ButtonProps) {
+  const theme = useContext(ThemeContext);
+  return (
+    <TouchableOpacity
+      style={
+        isOrange
+          ? Styles.btnOrange
+          : isGray
+          ? Styles.btnGray
+          : theme === "light"
+          ? Styles.btnLight
+          : Styles.btnDark
+      }
+      onPress={onPress}
+    >
+      <Text
+        style={
+          isOrange || isGray
+            ? Styles.smallTextLight
+            : theme === "dark"
+            ? Styles.smallTextLight
+            : Styles.smallTextDark
+        }
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
